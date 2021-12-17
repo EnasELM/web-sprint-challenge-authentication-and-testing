7184b15405d6b03a222e881a9cea401e35732edd
Enas <enaselmahdwi@gmail.com>
2021-12-17 07:59:32 -0700

    up

--- api/server.test.js
+++ api/server.test.js
@@ -26,34 +26,42 @@ describe('server.js', () => {
       expect(res.body.message).toMatch(/invalid credentials/i)
       expect(res.status).toBe(401)
     }, 750)
+    it('[2] responds with the correct status and message on missing usernamee or password', async () => {
+      let res = await request(server).post('/api/auth/login').send({ username: 'yasee' })
+      expect(res.body.message).toMatch(/username and password required/i)
+      expect(res.status).toBe(422)
+      res = await request(server).post('/api/auth/login').send({ password: '12345' })
+      expect(res.body.message).toMatch(/username and password required/i)
+      expect(res.status).toBe(422)
+    }, 750)
     
   })
   describe('[POST] /api/auth/register', () => {
-    it('[2] creates a new user in the database when client provide username and password', async () => {
+    it('[3] creates a new user in the database when client provide username and password', async () => {
       await request(server).post('/api/auth/register').send({ username: 'enas', password: '1234' })
       const name = await db('users').where('username', 'enas').first()
       expect(name).toMatchObject({ username: 'enas' })
     }, 750)
-    it('[3] creates a new user in the database when client does not provide  password', async () => {
+    it('[4] creates a new user in the database when client does not provide  password', async () => {
      let res = await request(server).post('/api/auth/register').send({ username: 'sara' })
      expect(res.body.message).toMatch(/username and password required/i)
     }, 750)
-    it('[4] creates a new user in the database when client does not provide username', async () => {
+    it('[5] creates a new user in the database when client does not provide username', async () => {
       let res = await request(server).post('/api/auth/register').send({ password: '1234' })
       expect(res.body.message).toMatch(/username and password required/i)
      }, 750)
-     it('[5] saves the user with a bcrypted', async () => {
+     it('[6] saves the user with a bcrypted', async () => {
       await request(server).post('/api/auth/register').send({ username: 'fafi', password: '1234' })
       const name = await db('users').where('username', 'fafi').first()
       expect(bcrypt.compareSync('1234', name.password)).toBeTruthy()
     }, 750)
  })
  describe('[GET] /api/jokes', () => {
-    it('[6] requests without a token ', async () => {
+    it('[7] requests without a token ', async () => {
      const res = await request(server).get('/api/jokes')
      expect(res.body.message).toMatch(/token required/i)
     }, 750)
-    it('[7] requests with an invalid token ', async () => {
+    it('[8] requests with an invalid token ', async () => {
      const res = await request(server).get('/api/jokes').set('Authorization', 'awad')
      expect(res.body.message).toMatch(/token invalid/i)
   }, 750)

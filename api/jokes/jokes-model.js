a261dc5e9d57e20132906b24d70d2f4d8f08ccee
Enas <enaselmahdwi@gmail.com>
2021-12-16 20:58:04 -0700

    up

--- api/jokes/jokes-model.js
+++ api/jokes/jokes-model.js
@@ -2,21 +2,21 @@ const db = require('../../data/dbConfig')
 
 function find() {
     return db('users').select('id', 'username')
- }
+}
 
- function findBy(filter) {
+function findBy(filter) {
     return db('users').where(filter)
-  }
+}
 
-  function findById(user_id) {
+function findById(user_id) {
     return db('users')
-    .select('id', 'username','password')
-    .where('id',user_id).first()
-  }
+     .select('id', 'username','password')
+     .where('id',user_id).first()
+}
 
-  async function add(user) {
+async function add(user) {
     const [id] = await db('users').insert(user)
     return findById(id)
-  }
+}
   
-  module.exports = { find, findBy, findById, add }
+module.exports = { find, findBy, findById, add }
